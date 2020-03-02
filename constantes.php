a6277d66bc7e6db56d86031f05992c1b1c88aba9
Valdemar <valdemarterroso.esaf@gmail.com>
2020-03-02 09:08:38 -0800

    constantes adicionado

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+    define("NOME_SITE", "XXXXX");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Constantes PHP </title>
+    </head>
+        <body>
+            <h1> Bem vindo ao site <?php echo NOME_SITE ?>!</h1>
+        </body>
+</html>

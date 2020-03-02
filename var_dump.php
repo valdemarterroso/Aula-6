70706967c47cec632a6ffb2f1295c11fa3e5e8a9
Valdemar <valdemarterroso.esaf@gmail.com>
2020-03-02 09:14:18 -0800

    var_dump adicionado

---
+++ var_dump.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> var_dump </title>
+</head>
+<body>
+    <?php
+        $texto = "Hello World!";
+        $numeroInteiro = 123;
+
+        var_dump($texto);
+        echo "<br>";
+
+        var_dump($numeroInteiro);
+    ?>
+    
+</body>
+</html>

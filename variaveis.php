d2dbc0cedb7205b0fd2a322d8ed749f1323d0555
Valdemar <valdemarterroso.esaf@gmail.com>
2020-03-02 08:59:29 -0800

    varáveis php adicionado

---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title> Variáveis PHP </title>
+</head>
+<body>
+    <?PHP
+        $texto = "Hello World!";
+        $numero = 33;
+
+        echo "O texto é: ". $texto;
+        echo "<br>";
+        echo "O número é:". $numero;
+    ?>
+    <br><br>
+    O texto é: <?php echo $texto ?>
+    <br>
+    O número é: <?php echo $numero ?>
+</body>
+</html>
